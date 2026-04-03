cf66ef760d36a9d9f748a36d0117443684dc52c3
Terry <agnel.terry@gmail.com>
2026-04-03 12:20:54 -0700

    Initial Commit

---
+++ SenseHat_dashboard/SenseHat_dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenseHat_dashboardApp.swift
+//  SenseHat_dashboard
+//
+//  Created by Agnelterry Thiburcius on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SenseHat_dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
